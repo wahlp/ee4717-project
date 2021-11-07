510e7ed3995d1cf657f0aef69907c73c2dc23812
wahlp <wahlp322@gmail.com>
2021-11-07 19:28:19 +0800

    Fixed redirects for vbox

--- php/redirect.php
+++ php/redirect.php
@@ -1,17 +1,13 @@
 <?php
     // utility to redirect user to another page without all this boilerplate in every file
 
-    // create full url with absolute path
-    // fixes broken redirects in xampp due to subfolder paths
-    $host_url = 'http://' . $_SERVER['HTTP_HOST']; 
-    $subdir = dirname($_SERVER['PHP_SELF'], 2);
-    if ($subdir == '\\') {
-        $subdir = '';
-    }
-
     function redirectTo($page) {
-        global $host_url, $subdir;
-        header('Location: ' . $host_url . $subdir . $page);
+        // check if we are running the project outside of xampp (in virtualbox)
+        if (strpos($_SERVER['DOCUMENT_ROOT'], 'xampp') === false) {
+            // fix the target url to include the apache subfolder path
+            $page = '/f32ee/ee4717-project' . $page;
+        }
+        header('Location: ' . $page);
         exit;
     }
 ?>
